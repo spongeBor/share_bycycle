20683405d4d77459465e94f8982a75f4d7156dd0
guoyubo <guoyubo@codemao.cn>
2021-10-12 09:32:41 +0800

    add resource

--- src/admin.js
+++ src/admin.js
@@ -1,22 +1,25 @@
 import { Col, Row} from 'antd'
 import Header from './components/Header'
 import Footer from './components/Footer'
+import NavLeft from './components/NavLeft'
 import React from 'react'
+import './style/common.less'
 export default class Admin extends React.Component {
   render() {
     return (
-      <Row>
-        <Col span='3'>Left</Col>
-        <Col span='21'>
+      <Row className='container'>
+        <Col span='4' className='nav-left'>
+          <NavLeft/>
+        </Col>
+        <Col span='20' className='main'>
           <Header>
             header
           </Header>
-          <Row>
+          <Row className='content'>
             content
+            {/* {this.props.children} */}
           </Row>
-          <Footer>
-            footer
-          </Footer>
+          <Footer/>
         </Col>
       </Row>
     )
